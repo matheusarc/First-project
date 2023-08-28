aab9fd09b61bf06692881f6ad6e5fe2503528996
math <matheuscostahr@gmail.com>
2023-08-27 21:10:07 -0300

    adicting my inf

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-function toggleMode () {
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
   /* pegar a tag img */
   const img = document.querySelector("#profile img")
   // substituir a img
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a img light
-    img.setAttribute('src', './Assets/assets/avatar-light.png')
+    img.setAttribute("src", "./Assets/assets/avatar1-light.png")
   } else {
     // se tiver night mode, adicionar a img night
-    img.setAttribute("src", "./Assets/assets/avatar.png")
+    img.setAttribute("src", "./Assets/assets/avatar1.png")
   }
   const alt = document.querySelector("#profile img")
-  
-  if(html.classList.contains('light')) {
-    html.setAttribute("alt", "foto do Mayk sorrindo, usando óculos de sol e de camisa preta, com o fundo colorido")
-  } else {
-    html.setAttribute("alt", "foto do Mayk sorrindo, usando óculos e de camisa preta, com o fundo amarelo")
-  }
 }
